4890a9afd2530f1b6f3cc7a1f36fd82c7b6b5a56
Andreas Philippi <18646892+andreasphil@users.noreply.github.com>
2021-07-08 21:09:16 +0200

    Cleanup

--- vite.config.js
+++ vite.config.js
@@ -11,12 +11,4 @@ export default defineConfig({
       "/@": path.resolve(__dirname, "/src"),
     },
   },
-  server: {
-    proxy: {
-      "^/api/.*": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
-      },
-    },
-  },
 })

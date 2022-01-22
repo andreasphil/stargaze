dcbaad36ce55e95fae59a262acaa0084695999c9
Andreas Philippi <18646892+andreasphil@users.noreply.github.com>
2022-01-22 10:27:51 +0100

    Use layers for additional Tailwind styles

--- src/App.vue
+++ src/App.vue
@@ -16,22 +16,23 @@ export default defineComponent({
 <style lang="postcss">
 @tailwind base;
 @tailwind components;
-
-body {
-  @apply text-gray-800 font-sans antialiased bg-white;
-}
-
-a {
-  @apply text-gray-500 underline transition-colors duration-150;
-}
-
-a:hover {
-  @apply text-gray-900;
-}
-
-svg {
-  @apply h-text-5;
-}
-
 @tailwind utilities;
+
+@layer base {
+  body {
+    @apply text-gray-800 font-sans antialiased bg-white;
+  }
+
+  a {
+    @apply text-gray-500 underline transition-colors duration-150;
+  }
+
+  a:hover {
+    @apply text-gray-900;
+  }
+
+  svg {
+    @apply h-text-5;
+  }
+}
 </style>

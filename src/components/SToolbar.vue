05e2e2ae151f4399c617e28c17887b7533cbdb8e
Andreas Philippi <18646892+andreasphil@users.noreply.github.com>
2022-03-26 14:55:55 +0100

    Make toolbar always appear distinct from background

--- src/components/SToolbar.vue
+++ src/components/SToolbar.vue
@@ -1,29 +1,6 @@
-<script lang="ts" setup>
-import { computed, onBeforeUnmount, onMounted, ref } from "vue";
-
-const floating = ref(false);
-
-const className = computed(() =>
-  floating.value ? "shadow-medium bg-$c-surface-bg" : "bg-$c-bg"
-);
-
-const updateFloatingState = () => {
-  floating.value = window.scrollY > 48;
-};
-
-onMounted(() => {
-  document.addEventListener("scroll", updateFloatingState, { passive: true });
-});
-
-onBeforeUnmount(() => {
-  document.removeEventListener("scroll", updateFloatingState);
-});
-</script>
-
 <template>
   <header
-    :class="className"
-    class="fixed top-0 left-0 right-0 h-20 flex items-center px-4"
+    class="fixed top-0 left-0 right-0 h-20 flex items-center px-4 shadow-medium bg-$c-surface-bg"
     f-transition
   >
     <nav f-container class="w-full flex-nowrap">

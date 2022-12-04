05b1144132c891ad7c706fe1fb0090b0c9628d02
Andreas Philippi <18646892+andreasphil@users.noreply.github.com>
2022-12-04 12:05:15 +0100

    Cleanup

--- functions/logout.ts
+++
@@ -1,9 +0,0 @@
-import type { Handler } from "@netlify/functions"
-import { setLoginCookie } from "./utils/cookie"
-
-export const handler: Handler = async () => {
-  return {
-    statusCode: 200,
-    headers: setLoginCookie(undefined),
-  }
-}

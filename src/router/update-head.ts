db3257e2fa3f50fe7b29714555acd4a92c26e349
Andreas Philippi <18646892+andreasphil@users.noreply.github.com>
2022-12-04 12:05:15 +0100

    Cleanup

--- src/router/update-head.ts
+++
@@ -1,4 +0,0 @@
-/** Applies values defined in the `opts` parameter to the document head. */
-export default function updateHead(opts: { title: string }) {
-  document.title = opts.title;
-}
--- src/utils/metadata.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  repository: "https://github.com/andreasphil/stargaze",
-  authorWebsite: "https://andreasphil.com",
-  author: "Andreas Philippi",
-  title: (page?: string) => `${page ? `${page} | ` : ""}Stargaze`,
-};

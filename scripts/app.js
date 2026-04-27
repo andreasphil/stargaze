568abf18f29fa226a5c16704f588d0c284c5ac02
Andreas Philippi <18646892+andreasphil@users.noreply.github.com>
2026-04-27 20:36:13 +0200

    feat: some visual tweaking

--- scripts/app.js
+++ scripts/app.js
@@ -32,17 +32,56 @@ export const Home = defineComponent({
       <label for="ghUsername" class="visually-hidden">Your GitHub username</label>
       <input
         autofocus
+        class="surface"
         id="ghUsername"
         placeholder="Your GitHub username"
         type="text"
         v-model="usernameModel"
       />
-      <button class="username-submit" variant="secondary" type="submit">🐱 Browse stars</button>
+      <button class="username-submit" variant="haptic" type="submit">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="lucide lucide-star-icon lucide-star"
+        >
+          <path
+            d="M11.525 2.295a.53.53 0 0 1 .95 0l2.31 4.679a2.123 2.123 0 0 0 1.595 1.16l5.166.756a.53.53 0 0 1 .294.904l-3.736 3.638a2.123 2.123 0 0 0-.611 1.878l.882 5.14a.53.53 0 0 1-.771.56l-4.618-2.428a2.122 2.122 0 0 0-1.973 0L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.122 2.122 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.122 2.122 0 0 0 1.597-1.16z"
+          />
+        </svg>
+        Browse stars
+      </button>
     </form>
 
     <div class="callout">
-      <div class="calloutIcon">💡</div>
-      <p class="callout-text">
+      <div class="color-primary">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="lucide lucide-lightbulb-icon lucide-lightbulb"
+        >
+          <path
+            d="M15 14c.2-1 .7-1.7 1.5-2.5 1-.9 1.5-2.2 1.5-3.5A6 6 0 0 0 6 8c0 1 .2 2.2 1.5 3.5.7.7 1.3 1.5 1.5 2.5"
+          />
+          <path d="M9 18h6" />
+          <path d="M10 22h4" />
+        </svg>
+      </div>
+
+      <p class="color-variant font-size-small no-margin">
         This uses GitHub&rsquo;s public API and doesn&rsquo;t require you to sign in. Because we
         rely on public information, Stargaze won&rsquo;t work with private profiles.
       </p>
